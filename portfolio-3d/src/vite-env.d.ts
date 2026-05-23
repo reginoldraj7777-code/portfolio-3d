a94d2ed5ee307bd849c7059b58ceb96bcdd56e91
Reginold Raj Malge <reginoldraj7777@gmail.com>
2026-05-23 14:28:00 +0000

    Fix Vercel build

--- portfolio-3d/src/vite-env.d.ts
+++ portfolio-3d/src/vite-env.d.ts
@@ -1,14 +1,1 @@
 /// <reference types="vite/client" />
-/// <reference types="@react-three/fiber" />
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      ambientLight: any;
-      pointLight: any;
-      mesh: any;
-      boxGeometry: any;
-      meshStandardMaterial: any;
-    }
-  }
-}
